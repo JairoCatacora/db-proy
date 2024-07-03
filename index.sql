7fa8b0412f0f25fe79604ebcf49155b19bb7ce3e
JairoCatacora <alair.catacora@utec.edu.pem>
2024-07-03 15:49:02 -0500

    c2

---
+++ index.sql
@@ -0,0 +1,10 @@
+--Query 1:
+CREATE INDEX index _flanzamiento ON publicacion USING btree(fecha_lanzamiento);
+CREATE INDEX index_ncategoria ON tiene USING btree(nom_categoria ) ;
+
+--Query 2:
+CREATE INDEX index_fcompra ON compra USING btree(fecha_compra);
+CREATE INDEX index_pais ON compra USING hash(pais);
+
+--Query 3:
+CREATE INDEX index_fareciente ON biblioteca USING btree( f_act_reciente );
