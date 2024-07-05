351560a6f4043aaa4423b6981dc2fccc848571c7
JairoCatacora <112722355+JairoCatacora@users.noreply.github.com>
2024-07-05 18:43:44 -0500

    Update index.sql

--- index.sql
+++ index.sql
@@ -3,6 +3,8 @@ CREATE INDEX index _flanzamiento ON publicacion USING btree(fecha_lanzamiento);
 CREATE INDEX index_ncategoria ON tiene USING btree(nom_categoria ) ;
 
 --Query 2:
+CREATE INDEX index_anholanz ON publicacion USING hash(fecha_lanzamiento);
+CREATE INDEX index_precio ON publicacion USING hash(precio);
 CREATE INDEX index_fcompra ON compra USING btree(fecha_compra);
 CREATE INDEX index_pais ON compra USING hash(pais);
 
